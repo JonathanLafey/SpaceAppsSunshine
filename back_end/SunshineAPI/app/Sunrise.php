196fae54e7385bf8f792012c9f3c2597672c6b87
AVovchenko <alexander.vovchenko@gmail.com>
2017-04-30 00:00:10 +0300

    fake sunrise and sunset times

--- back_end/SunshineAPI/app/Sunrise.php
+++ back_end/SunshineAPI/app/Sunrise.php
@@ -8,4 +8,8 @@ class Sunrise extends Model
     protected $primaryKey = 'id';
     protected $table = 'sunrise';
     protected $guarded = [];
+
+    public function getSunriseTime($lat, $lng){
+        return "06:00";
+    }
 }
--- back_end/SunshineAPI/app/Sunset.php
+++ back_end/SunshineAPI/app/Sunset.php
@@ -8,4 +8,8 @@ class Sunset extends Model
     protected $primaryKey = 'id';
     protected $table = 'sunset';
     protected $guarded = [];
+
+    public function getSunsetTime($lat, $lng){
+        return "18:00";
+    }
 }
