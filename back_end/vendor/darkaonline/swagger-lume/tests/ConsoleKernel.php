f42c4e093597d4ec56612da08edf7c6890edc04c
Dimitris Argyroudis <xidatosar@hotmail.gr>
2017-04-29 20:26:47 +0300

    setup initial migration

---
+++ back_end/vendor/darkaonline/swagger-lume/tests/ConsoleKernel.php
@@ -0,0 +1,28 @@
+<?php
+
+use Laravel\Lumen\Console\Kernel;
+use Illuminate\Console\Scheduling\Schedule;
+
+class ConsoleKernel extends Kernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        //
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     *
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        //
+    }
+}
---
+++ back_end/vendor/darkaonline/swagger-lume/tests/RoutesTest.php
@@ -0,0 +1,40 @@
+<?php
+
+class RoutesTest extends LumenTestCase
+{
+    /** @test */
+    public function cant_access_json_file()
+    {
+        $jsonUrl = config('swagger-lume.routes.docs');
+
+        $this->get($jsonUrl);
+
+        $this->assertResponseStatus(404);
+    }
+
+    /** @test */
+    public function can_access_json_file()
+    {
+        $jsonUrl = config('swagger-lume.routes.docs');
+
+        $this->setPaths()->crateJsonDocumentationFile();
+
+        $this->get($jsonUrl);
+
+        $this->assertResponseOk();
+    }
+
+    /** @test */
+    public function can_access_documentation_interface()
+    {
+        $this->setPaths();
+
+        $this->app->prepareForConsoleCommand();
+
+        $response = $this->get(config('swagger-lume.routes.api'));
+
+        $this->assertResponseOk();
+
+        $this->assertContains($this->validator_url, $response->response->getContent());
+    }
+}
