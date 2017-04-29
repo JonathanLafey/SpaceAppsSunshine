b01be583baf9ef77449e9704a1a012b7ef9e2a82
Dimitris Argyroudis <xidatosar@hotmail.gr>
2017-04-29 22:57:18 +0300

    initial api setup

---
+++ back_end/SunshineAPI/app/Http/Controllers/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+  
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+  
+class MainController extends Controller{
+
+    public function index(){
+        return response()->json('Hello World');
+    }
+
+    public function give_prediction(Request $request){
+        // TODO: call prediction functionality here (service)
+        return response()->json($request)
+    }
+
+}
+
+?>
