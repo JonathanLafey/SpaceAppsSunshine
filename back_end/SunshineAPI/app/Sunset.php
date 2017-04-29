3caf937c1541ee59d9e96d4f02753405bdaf1a28
AVovchenko <alexander.vovchenko@gmail.com>
2017-04-30 00:49:06 +0300

    sunrise and sunset times

--- back_end/SunshineAPI/app/Sunset.php
+++ back_end/SunshineAPI/app/Sunset.php
@@ -9,7 +9,14 @@ class Sunset extends Model
     protected $table = 'sunset';
     protected $guarded = [];
 
-    public function getSunsetTime($lat, $lng){
-        return "18:00";
+    public static function getSunsetTime($lat, $lng){
+        $display = file_get_contents('http://api.openweathermap.org/data/2.5/weather?lat='.$lat.'&lon='.$lng.'&appid=18a2b1bef5eb5f76f35776ae10ede502');
+        $output= json_decode($display, true);
+        if (!empty($output["sys"]) and !empty($output["sys"]["sunset"])){
+            return date('Y-m-d H:i:s', $output["sys"]["sunset"]);
+        }else{
+            return date("Y-m-d 19:00");
+        }
     }
+
 }
