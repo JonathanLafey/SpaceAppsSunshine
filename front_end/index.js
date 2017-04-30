5464624212582f8a4fdc4bcb99a2ddb57498e723
Paulo Madronero <madroneropaulo@hotmail.com>
2017-04-30 13:28:05 +0300

    Send data to server, visualize data

--- front_end/index.js
+++ front_end/index.js
@@ -5,6 +5,7 @@ var SETUP_TYPE;
     SETUP_TYPE[SETUP_TYPE["POLY"] = 2] = "POLY";
 })(SETUP_TYPE || (SETUP_TYPE = {}));
 var mapCoordinates;
+var graph;
 function initApp() {
     initializeListeners();
 }
@@ -12,11 +13,108 @@ function initializeListeners() {
     var estimate = document.getElementById("estimate");
     estimate.onclick = function () {
         var data = getUserData();
-        console.log('Data to send:', data);
+        var r = new XMLHttpRequest();
+        r.open("POST", "http://ec2-52-87-237-84.compute-1.amazonaws.com:90/prediction", true);
+        r.onreadystatechange = function (res) {
+            if (r.readyState !== XMLHttpRequest.DONE || r.status != 200) {
+                console.error('ERROR!!', res);
+                return;
+            }
+            ;
+            var response = r.responseText;
+            var formatedResponse = JSON.parse(response);
+            alert("Success: " + formatedResponse);
+            var estimatesRes = formatedResponse.estimates;
+            drawData(estimatesRes);
+            updateView(estimatesRes);
+        };
+        r.send(JSON.stringify(data));
+        //let mockData =  [
+        //        {
+        //            timestamp: 1493517042,
+        //            estimate: 3.45
+        //        },
+        //        {
+        //            timestamp: 1493632242,
+        //            estimate: 3.17
+        //        },
+        //        {
+        //            timestamp: 1493718642,
+        //            estimate: 4.02
+        //        },
+        //        {
+        //            timestamp: 1493718642,
+        //            estimate: 3.58
+        //        },
+        //        {
+        //            timestamp: 1493718642,
+        //            estimate: 3.97
+        //        },
+        //        {
+        //            timestamp: 1493718642,
+        //            estimate: 3.36
+        //        },
+        //        {
+        //            timestamp: 1493718642,
+        //            estimate: 3.48
+        //        }
+        //        ];
+        //drawData(mockData);
+        //updateView(mockData);
+    };
+    var sendFeedback = document.getElementById("estimate-help");
+    sendFeedback.onclick = function () {
+        var feedbackSection = document.getElementById("help-feedback");
+        feedbackSection.style.display = 'block';
     };
 }
+function updateView(estimates) {
+    var calculationRes = document.getElementById("after-data");
+    calculationRes.style.display = 'block';
+    var calculationText = document.getElementById("calculated-energy");
+    var today = new Date();
+    calculationText.innerText = "The approximate maximum of energy that will be produced on: " + today.getDate() + "/" + today.getMonth() + " is " + estimates[0].estimate + " Kwh";
+}
+function drawData(estimates) {
+    var graphicsViewContainer = document.getElementById("graphics-view");
+    graphicsViewContainer.innerHTML = '';
+    var graphDom = createDom(estimates);
+    graphicsViewContainer.appendChild(graphDom);
+}
+function createDom(estimates) {
+    var listParent = document.createElement("ul");
+    listParent.setAttribute("class", "graph");
+    var maxEstimation = getMaxLength(estimates);
+    for (var i = 0; i < estimates.length; i++) {
+        var dateInfo = new Date(estimates[i].timestamp * 1000);
+        var estimateVal = estimates[i].estimate;
+        var estimateItemContainer = document.createElement("li");
+        estimateItemContainer.setAttribute("class", "graph_item-container");
+        var itemLabel = document.createElement("p");
+        itemLabel.innerText = "Estimated energy produced on " + (dateInfo.getDay() + 1) + "/" + (dateInfo.getMonth() + 1) + ": " + estimateVal + "Kwh";
+        estimateItemContainer.appendChild(itemLabel);
+        var estimateItem = document.createElement("div");
+        estimateItem.setAttribute("class", "graph_item-container__item");
+        var widthInPercent = Math.floor(estimateVal / maxEstimation * 100);
+        var widthVal = widthInPercent + "%";
+        estimateItem.style.width = widthVal;
+        estimateItemContainer.appendChild(estimateItem);
+        listParent.appendChild(estimateItemContainer);
+    }
+    return listParent;
+}
+function getMaxLength(estimates) {
+    return estimates.reduce(function (prev, curr) {
+        if (curr.estimate > prev) {
+            return curr.estimate;
+        }
+        else {
+            return prev;
+        }
+    }, 0);
+}
 function getUserData() {
-    var lat = mapCoordinates.lat, lng = mapCoordinates.lng;
+    var _a = mapCoordinates || { lat: 0, lng: 0 }, lat = _a.lat, lng = _a.lng;
     var locationData = { lat: lat, lng: lng };
     var setupData = getSetupData();
     var formData = {
