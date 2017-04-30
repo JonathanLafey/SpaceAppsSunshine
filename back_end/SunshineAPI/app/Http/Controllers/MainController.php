ea26293e52248c8c0fac25d969ea15c75b29a0d2
Dimitris Argyroudis <xidatosar@hotmail.gr>
2017-04-30 13:29:20 +0300

    fix error with var_dump

--- back_end/SunshineAPI/app/Http/Controllers/MainController.php
+++ back_end/SunshineAPI/app/Http/Controllers/MainController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request; 
 use App\SolarRadiation;
 use App\Temperature;
+use App\Sunrise;
+use App\Sunset;
   
 class MainController extends Controller{
 
@@ -21,17 +23,17 @@ class MainController extends Controller{
 //	$area = $data['setup']['area'];
 	$date = '2016-09-30';
 
-
+	$sunrise_a= str_split(Sunrise::query()->whereLocaldate($date)->avg('numeric_value'));
+	$sunrise = '0'.$sunrise_a[0].':'.$sunrise_a[1].$sunrise_a[2].':00';
+	$sunset_a = str_split(Sunset::query()->whereLocaldate($date)->avg('numeric_value'));
+	$sunset = $sunset_a[0].$sunset_a[1].':'.$sunset_a[2].$sunset_a[3].':00';
 	$radiation = (float)SolarRadiation::query()->whereLocaldate($date)->avg('numeric_value');
 	$temperature = (float)Temperature::query()->whereLocaldate($date)->avg('numeric_value');
-//	$sunrise= (float)Sunrise::query()->whereLocaldate($date)->avg('numeric_value');
-//	$sunset = (float)Sunset::query()->whereLocaldate($date)->avg('numeric_value');
 
 	$area = (float)$data['setup']['area'];
 	$type = (int)$data['setup']['type'];
 	$panel_efficiency = ($type == 0 ? 0.08 : 0.16);
 	$temperature_formula = ((($temperature-32)/1.8) - 25) * -0.01;
-	var_dump($temperature_formula);
 	$output = $radiation * $area * ($panel_efficiency * (1 + $temperature_formula));
 
 	$c_date = '2017-04-30';	
